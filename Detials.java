c9ead3c16ba2782e8aaf25a4838ae9a0165dd7f5
shanjana <shanjanars.20cse@kongu.edu>
2021-10-24 10:15:20 +0530

    Insurance commit

---
+++ Detials.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class details {
+      public static void main(String args[])
+      {
+           
+          Scanner SC=new Scanner(System.in);
+           
+          System.out.print(" Name: ");
+           String name=SC.nextLine();
+           System.out.print("  Father Name: ");
+           String FatherName=SC.nextLine();
+           System.out.print("  MotherName: ");
+           String MotherName=SC.nextLine();
+           System.out.print(" MobileNumber: " );
+           int MobileNumber=SC.nextInt();
+           System.out.print(" Age: ");
+           int Age=SC.nextInt();
+           System.out.print("  Email: ");
+           String Email=SC.nextLine();
+           System.out.print(" Bloodgroup: ");
+           int Bloodgroup=SC.nextInt();
+           System.out.print("  Address: ");
+           String Address=SC.nextLine();
+           System.out.print(" Aadhar Number: ");
+           int AadharNumber=SC.nextInt();
+
+
+
+         
+      }
+          
+}
+
+    
+
---
+++ indeetials.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class indeetials
+ {
+
+      public static void main(String args[])
+      {
+           
+          Scanner SC=new Scanner(System.in);
+           
+          System.out.print("  StudentName: ");
+           String StudentName=SC.nextLine();
+           System.out.print("  NomineeName: ");
+           String NomineeName=SC.nextLine();
+           System.out.print("  Releation of Nominee: ");
+           String ReleationofNominee=SC.nextLine();
+           System.out.print("  NomineeMobileNumber: " );
+           int  NomineeMobileNumber=SC.nextInt();
+           System.out.print("  NomineeAge: ");
+           int  NomineeAge=SC.nextInt();
+           System.out.print("  Email: ");
+           String Email=SC.nextLine();
+           System.out.print("  Occuption: ");
+           String  Occuption=SC.nextLine();
+           System.out.print(" NomineeAadhar Number: ");
+           int NomineeAadharNumber=SC.nextInt();
+
+
+
+         
+      }
+    }
