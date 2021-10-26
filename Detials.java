0a0d9a04c9bc8e58ae5d807c3d5f2f8fd317c89e
shanjana <shanjanars.20cse@kongu.edu>
2021-10-26 14:41:11 +0530

    course complete

--- Detials.java
+++ Detials.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class details {
+public class Details {
       public static void main(String args[])
       {
            
---
+++ coursedetials.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class coursedetials {
+    public static void main(String args[]){
+        Scanner SC=new Scanner(System.in);
+         System.out.print(" Roll no:");
+         String Rollno=SC.nextLine();
+         System.out.print("  Name  of one credit courses completed : ");
+         String Nameofonecreditcoursescompleted=SC.nextLine();
+         System.out.print(" Name of the value added courses completed   : ");
+         String Nameofthevalueaddedcoursescompleted=SC.nextLine();
+         System.out.print("  No  one credit courses completed   : ");
+         int Noofonecreditcoursescompleted=SC.nextInt();
+         System.out.print(" No  the value added courses completed : ");
+         int Nothevalueaddedcoursescompleted =SC.nextInt();
+
+
+    }
+    
+}
